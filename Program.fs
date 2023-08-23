338629dcc0dd759b4eb0dd5b98421591406e8bf1
Johanx22x <johanrodsa2210@gmail.com>
2023-08-23 17:55:00 -0600

    Initial project structure

---
+++ Program.fs
@@ -0,0 +1,58 @@
+﻿namespace SharpTune
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+open Avalonia.Controls
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.Layout
+
+module Main =
+
+    let view () =
+        Component(fun ctx ->
+            let state = ctx.useState "Hello World!"
+
+            DockPanel.create [
+                DockPanel.children [
+                    TextBlock.create [
+                        TextBlock.dock Dock.Top
+                        TextBlock.fontSize 48.0
+                        TextBlock.verticalAlignment VerticalAlignment.Center
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (string state.Current)
+                    ]
+                ]
+            ]
+        )
+
+type MainWindow() =
+    inherit HostWindow()
+    do
+        base.Title <- "SharpTune"
+        base.Content <- Main.view()
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add (FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+
+    [<EntryPoint>]
+    let main(args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+  <img src="docs/logo.png" alt="Logo" width="150" height="150">
+</p>
+
+<h1 align="center">
+    SharpBeat
+</h1>
+
+Welcome to SharpBeat repository! This project aims to provide a frontend
+application for a music streaming application using the F# programming
+language. It allows you to manage songs, albums, playlists, and more.
+
+This is part of a project for my Programming Languages course at Costa Rica 
+Institute of Technology.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues for bug reports, feature
+requests, or any questions you might have. Pull requests are also accepted.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file
+for details.
